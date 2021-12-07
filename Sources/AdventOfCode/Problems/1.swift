251dc21ba855fc887ef6a4904d55ebe57a3c73d4
Anton Sergeev <antonsergeev88@gmail.com>
2021-12-07 07:26:00 +0300

    Add part 2 of day 1

--- Sources/AdventOfCode/Problems/1.swift
+++ Sources/AdventOfCode/Problems/1.swift
@@ -14,13 +14,59 @@ struct Day1: Problem {
         return text.split(separator: "\n").map(String.init).compactMap(Int.init)
     }
 
-    func process(_ input: [Int]) async throws -> Int {
-        zip(input.dropLast(1), input.dropFirst(1)).reduce(0) { partialResult, pair in
-            pair.0 < pair.1 ? partialResult + 1 : partialResult
-        }
+    func process(_ input: [Int]) async throws -> (first: Int, second: Int) {
+        let first = input
+            .slidingWindow(size: 2)
+            .reduce(0) { partialResult, pair in
+                pair[0] < pair[1] ? partialResult + 1 : partialResult
+            }
+
+        let second = input
+            .slidingWindow(size: 3)
+            .map {
+                $0.reduce(0, +)
+            }
+            .slidingWindow(size: 2)
+            .reduce(0) { partialResult, pair in
+                pair[0] < pair[1] ? partialResult + 1 : partialResult
+            }
+
+        return (first: first, second: second)
     }
 
-    func text(from output: Int) -> String {
+    func text(from output: (first: Int, second: Int)) -> String {
         String(describing: output)
+    }
+}
+
+struct SlidingWindow<C>: Sequence, IteratorProtocol where C: Collection {
+    private let collection: C
+    private let windowSize: Int
+    private var index: C.Index
+
+    init(_ collection: C, windowSize: Int) {
+        self.collection = collection
+        self.windowSize = windowSize
+        self.index = collection.startIndex
+    }
+
+    func makeIterator() -> some IteratorProtocol {
+        self
+    }
+
+    mutating func next() -> [C.Element]? {
+        guard collection.index(index, offsetBy: windowSize) <= collection.endIndex else {
+            return nil
+        }
+        defer {
+            index = collection.index(after: index)
+        }
+        return Array(collection[index..<collection.index(index, offsetBy: windowSize)])
+    }
+}
+
+extension Collection {
+    func slidingWindow(size windowSize: Int) -> SlidingWindow<Self> {
+        .init(self, windowSize: windowSize)
     }
 }
