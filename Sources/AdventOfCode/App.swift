ba01b890f528d1556cf1fb16d6cb2f4f416e9e10
Anton Sergeev <antonsergeev88@gmail.com>
2021-12-13 07:28:17 +0300

    Add Day 5

--- Sources/AdventOfCode/App.swift
+++ Sources/AdventOfCode/App.swift
@@ -6,6 +6,7 @@ struct App {
             try await Day2().run(with: "2"),
             try await Day3().run(with: "3"),
             try await Day4().run(with: "4"),
+            try await Day5().run(with: "5"),
         ]
             .enumerated()
             .forEach { (offset, element) in
---
+++ Sources/AdventOfCode/Problems/5.swift
@@ -0,0 +1,138 @@
+import Foundation
+
+extension Day5 {
+    struct Point: Hashable {
+        let x: Int
+        let y: Int
+
+        static func with(input: String) -> Point {
+            let coordinates = input.split(separator: ",").map(String.init).compactMap(Int.init)
+            return .init(x: coordinates[0], y: coordinates[1])
+        }
+    }
+
+    struct Line {
+        let left: Point
+        let right: Point
+
+        static func with(input: String) -> Line {
+            let points = input.split(separator: " ").map(String.init).filter({ $0 != "->" }).map(Point.with(input:))
+            return .init(left: points[0], right: points[1])
+        }
+    }
+
+    struct DiagonalLine: Sequence, IteratorProtocol {
+        let start: Point
+        let end: Point
+        let diff: Point?
+        private var order = 0
+        init(_ line: Line) {
+            self.start = line.left
+            self.end = line.right
+            let diffX = line.right.x - line.left.x
+            let diffY = line.right.y - line.left.y
+            if abs(diffX) == abs(diffY), abs(diffX) > 0 {
+                self.diff = .init(x: diffX > 0 ? 1 : -1, y: diffY > 0 ? 1 : -1)
+            } else {
+                self.diff = nil
+            }
+        }
+        func makeIterator() -> Day5.DiagonalLine {
+            self
+        }
+        mutating func next() -> Point? {
+            guard let diff = diff else {
+                return nil
+            }
+            defer {
+                self.order += 1
+            }
+            if diff.x > 0 {
+                if start.x + diff.x * order <= end.x {
+                    return .init(x: start.x + diff.x * order, y: start.y + diff.y * order)
+                } else {
+                    return nil
+                }
+            } else {
+                if start.x + diff.x * order >= end.x {
+                    return .init(x: start.x + diff.x * order, y: start.y + diff.y * order)
+                } else {
+                    return nil
+                }
+            }
+        }
+    }
+
+    struct FlatLine: Sequence, IteratorProtocol {
+        let start: Point
+        let end: Point
+        private var order = 0
+        init(_ line: Line) {
+            if line.left.x == line.right.x {
+                start = line.left.y <= line.right.y ? line.left : line.right
+                end = line.left.y <= line.right.y ? line.right : line.left
+            } else if line.left.y == line.right.y {
+                start = line.left.x <= line.right.x ? line.left : line.right
+                end = line.left.x <= line.right.x ? line.right : line.left
+            } else {
+                start = line.left
+                end = line.right
+            }
+        }
+        func makeIterator() -> Day5.FlatLine {
+            self
+        }
+        mutating func next() -> Point? {
+            defer { order += 1 }
+            if start.x == end.x, start.y + order <= end.y {
+                return .init(x: start.x, y: start.y + order)
+            } else if start.y == end.y, start.x + order <= end.x {
+                return .init(x: start.x + order, y: start.y)
+            } else {
+                return nil
+            }
+        }
+    }
+}
+
+struct Day5: Problem {
+    func input(from stream: InputStream) throws -> [Line] {
+        var data = Data()
+        let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: 1024)
+        defer {
+            buffer.deallocate()
+        }
+        while stream.hasBytesAvailable {
+            let count = stream.read(buffer, maxLength: 1024)
+            data.append(buffer, count: count)
+        }
+        guard let text = String(data: data, encoding: .utf8) else {
+            throw ProblemError.badInput
+        }
+        return text.split(separator: "\n").map(String.init).map(Line.with(input:))
+    }
+
+    func process(_ input: [Line]) async throws -> (first: Int, second: Int) {
+        var map: [Point: Int] = [:]
+        for line in input {
+            for point in FlatLine(line) {
+                map[point] = map[point] == nil ? 1 : map[point]! + 1
+            }
+        }
+        let overlaps = map.filter { $0.value > 1 }
+
+        for line in input {
+            for point in DiagonalLine(line) {
+                map[point] = map[point] == nil ? 1 : map[point]! + 1
+            }
+        }
+
+        let allOverlaps = map.filter { $0.value > 1 }
+
+        return (overlaps.count, allOverlaps.count)
+    }
+
+    func text(from output: (first: Int, second: Int)) -> String {
+        String(describing: output)
+    }
+}
